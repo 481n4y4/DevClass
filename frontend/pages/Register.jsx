03d5a101cd81d1aed0272e0c24cd1a077c4f3541
izalllxyz-cell <izalllxyz@gmail.com>
2026-02-24 09:21:31 +0700

    update ui register

--- frontend/pages/Register.jsx
+++ frontend/pages/Register.jsx
@@ -155,6 +155,7 @@ export default function Register() {
                 Nama Lengkap
               </label>
               <div className="relative">
+                
                 <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
                   <svg className="h-5 w-5 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
